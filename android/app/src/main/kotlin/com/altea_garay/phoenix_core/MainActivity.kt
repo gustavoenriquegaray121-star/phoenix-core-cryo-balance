7e248f22931ba84fb89588c8a1943d337ed9350d
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-04-09 09:46:06 -0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/altea_garay/phoenix_core/MainActivity.kt
+++ android/app/src/main/kotlin/com/altea_garay/phoenix_core/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.altea_garay.phoenix_core
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import com.ryanheise.audioservice.AudioServicePlugin
 
-class MainActivity: FlutterActivity() {
-    override fun provideFlutterEngine(context: android.content.Context): FlutterEngine? {
-        return AudioServicePlugin.getFlutterEngine(context)
-    }
-}
+class MainActivity: FlutterActivity()
