357753428d7b68386679111d417e9e7a72016394
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-04-18 02:03:14 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -664,7 +664,11 @@ class _DockPainter extends CustomPainter {
     for (final p in particles) {
       final a = (p.life/p.maxLife).clamp(0.0,1.0);
       if (p.isSmoke) {
-        canvas.drawCircle(Offset(p.x-sw/2, p.y-sh*0.3), p.size);
+        final paint = Paint()
+  ..color = p.color.withOpacity(a * 0.4)
+  ..maskFilter = MaskFilter.blur(BlurStyle.normal, p.size * 0.8);
+
+canvas.drawCircle(Offset(p.x - sw/2, p.y - sh*0.3), p.size, paint);
             Paint()..color = p.color.withOpacity(a*0.4)..maskFilter = MaskFilter.blur(BlurStyle.normal, p.size*0.8);
       } else {
         canvas.drawCircle(Offset(p.x-sw/2, p.y-sh*0.3), p.size,
